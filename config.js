d836eb74d3f4e6052193267b07f854144f4eedbd
MdL <mdlmdel@gmail.com>
2017-03-14 17:47:33 -0400

    Remove database credentials from git

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ? 
-                        'mongodb://entity-sentiment-analysis:TestDB9787@ds111549.mlab.com:11549/entities-app':
-                      'mongodb://localhost/entity-sentiment-analysis');
+                      'mongodb://localhost/entity-sentiment-analysis';
 exports.PORT = process.env.PORT || 8080;
